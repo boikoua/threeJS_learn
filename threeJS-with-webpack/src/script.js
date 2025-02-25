346eaf4b6405a89c7705029db2567d26306800aa
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-25 10:19:54 +0200

    work with position, scale and axesHelper

--- threeJS-with-webpack/src/script.js
+++ threeJS-with-webpack/src/script.js
@@ -4,22 +4,38 @@ import './style.css';
 // Создание сцены
 const scene = new THREE.Scene();
 
+// Создаём график осей координат и добавляем его на сцену
+const axesHelper = new THREE.AxesHelper(3);
+scene.add(axesHelper);
+
 // Создаю сам объект (фигуру)
 const geometry = new THREE.BoxGeometry(1, 1, 1); // размеры фигуры
-const material = new THREE.MeshBasicMaterial({ color: 'purple' }); // материал фигуры (цвет, заливка)
+const material = new THREE.MeshBasicMaterial({
+  color: 'purple', // цвет фигуры
+  wireframe: true, // прозрачность фигуры
+}); // материал фигуры (цвет, заливка)
 const mesh = new THREE.Mesh(geometry, material); // создаю сетку, объеденив всё в один
+
+// Позиции для перемещения фигуры
+// mesh.position.x = -1;
+// mesh.position.y = -0.5;
+// mesh.position.z = 0.5;
+
+// Делаю масштабирование фигуры
+mesh.scale.x = 0.5;
+mesh.scale.y = 2;
+mesh.scale.z = 0.7;
 
 scene.add(mesh); // добавляю сетку на сцену
 
 // Создаю камеру (то как будет показана сама сцена)
 const sizes = {
-  width: 600,
-  height: 600,
+  width: 500,
+  height: 500,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); // указываю угол обзора и размеры самой камеры
 camera.position.z = 3; // смещаем камеру по оси Z
-camera.position.y = 1; // смещаем камеру по оси Y
 
 scene.add(camera); // добавляю камеру на сцену
 
