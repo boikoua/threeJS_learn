6b2151e8f7906e734ee25b4244a0b1a62e42fa56
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-25 14:39:54 +0200

    add Torus and Sphere

--- threeJS_gosha/index.js
+++ threeJS_gosha/index.js
@@ -27,8 +27,32 @@ const material = new THREE.MeshBasicMaterial({
 });
 
 const cube = new THREE.Mesh(geometry, material); // собираю куб в один меш
-
+cube.position.set(-3, 0, 0); // двигаем куб по XYZ
 scene.add(cube); // добавляю куб на сцену
+
+// Создаю стандартную сферу с радиусом сферы и сигментами (пересечения)
+const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
+const sphereMaterial = new THREE.MeshPhongMaterial({
+  color: 'blue',
+  emissive: 'white',
+  shininess: 100,
+});
+
+const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.set(2, 0, 0);
+scene.add(sphere);
+
+// создаю пончик (стандартный)
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.7, 0.2, 16, 100),
+  new THREE.MeshBasicMaterial({
+    color: 'orange',
+  })
+);
+
+torus.position.set(2, 2, 1);
+scene.add(torus);
 
 // делаю функцию, которая будет постоянно рендерить мою фигуру на страницу
 function animate() {
@@ -37,6 +61,12 @@ function animate() {
   // при каждом перерендере меняет свою позицию для вращения
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  sphere.rotation.x += 0.01;
+  sphere.rotation.y += 0.01;
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
