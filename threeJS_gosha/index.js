74d1bcc250885783a833ed85747a8f4c7110afe7
Dmytro Boiko <iamboikodmytro@gmail.com>
2025-02-25 20:11:20 +0200

    work with sources light

--- threeJS_gosha/index.js
+++ threeJS_gosha/index.js
@@ -2,6 +2,33 @@ import * as THREE from 'three';
 
 const scene = new THREE.Scene(); // создал сцену
 
+// Создаю источники света Light
+// Свет для всей сцены (создали и добавили на сцену) (похож на фильтр)
+// всегда указываем первым параметром цвет, вторым интенсивность света
+// const ambientLight = new THREE.AmbientLight('white', 0.1);
+// scene.add(ambientLight);
+
+// direction light свет который похож по типу солнца (светит с одной стороны которую мы укажем позиционированием)
+// всегда указываем первым параметром цвет, вторым интенсивность света
+// const dirLight = new THREE.DirectionalLight('white', 1);
+// dirLight.position.set(5, 5, 5);
+// scene.add(dirLight);
+
+// point light похож на эффект фонаря (с одной точки, будет рассеивать свет)
+// всегда указываем первым параметром цвет, вторым интенсивность света и дистанцию третим
+// const pointLight = new THREE.PointLight('white', 10, 100);
+// pointLight.position.set(0.5, 1, 1);
+// scene.add(pointLight);
+
+// визуализация света (нужна только на стадии разработки) тестируем
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.2);
+// scene.add(pointLightHelper);
+
+// spot light освещает только в одну сторону, как фонарь, но не рассеивает
+const spotLight = new THREE.SpotLight('white', 1);
+spotLight.position.set(1, 1, 1);
+scene.add(spotLight);
+
 // создал камеру
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -16,43 +43,22 @@ camera.position.z = 5; // переместил камеру по оси z
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-// добавил библиотеку на страницу
+// добавил рендер на страницу
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(); // создание заготовленого стандартного куба
 
 // указываю параметры для куба
-const material = new THREE.MeshBasicMaterial({
-  color: 'red',
-});
+// const material = new THREE.MeshBasicMaterial({
+//   color: 'red',
+// });
+
+// материал для куба, что бы работал свет общей сцены
+const material = new THREE.MeshStandardMaterial({ color: 'red' });
 
 const cube = new THREE.Mesh(geometry, material); // собираю куб в один меш
-cube.position.set(-3, 0, 0); // двигаем куб по XYZ
+cube.position.set(0, 0, 0); // двигаем куб по XYZ
 scene.add(cube); // добавляю куб на сцену
-
-// Создаю стандартную сферу с радиусом сферы и сигментами (пересечения)
-const sphereGeometry = new THREE.SphereGeometry(0.5, 32, 32);
-const sphereMaterial = new THREE.MeshPhongMaterial({
-  color: 'blue',
-  emissive: 'white',
-  shininess: 100,
-});
-
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-sphere.position.set(2, 0, 0);
-scene.add(sphere);
-
-// создаю пончик (стандартный)
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.7, 0.2, 16, 100),
-  new THREE.MeshBasicMaterial({
-    color: 'orange',
-  })
-);
-
-torus.position.set(2, 2, 1);
-scene.add(torus);
 
 // делаю функцию, которая будет постоянно рендерить мою фигуру на страницу
 function animate() {
@@ -61,12 +67,6 @@ function animate() {
   // при каждом перерендере меняет свою позицию для вращения
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
-
-  sphere.rotation.x += 0.01;
-  sphere.rotation.y += 0.01;
-
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
